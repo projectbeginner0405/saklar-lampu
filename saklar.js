205db74c0a615c68fa3ff46de6606e8c8a7f9e31
projectbeginner0405 <mohamadbh515@gmail.com>
2024-06-23 16:20:29 +0700

    Add files via upload

---
+++ saklar.js
@@ -0,0 +1,30 @@
+function saklar() {
+    let toggle1 = document.getElementById("default-toggle1");
+    let toggle2= document.getElementById("default-toggle2");
+    let toggle3 = document.getElementById("default-toggle3");
+
+    let lampu1 = document.getElementById("lampu1");
+    let lampu2 = document.getElementById("lampu2");
+    let lampu3 = document.getElementById("lampu3");
+
+    if (toggle1.checked) {
+        lampu1.src="asset/images/on.png"
+     } else {
+     lampu1.src="asset/images/off.png";
+   }
+
+   if (toggle2.checked) {
+    lampu2.src="asset/images/on.png"
+} else {
+ lampu2.src="asset/images/off.png";
+}
+
+if (toggle3.checked) {
+    lampu3.src="asset/images/on.png"
+} else {
+ lampu3.src="asset/images/off.png";
+}
+    }
+
+
+
